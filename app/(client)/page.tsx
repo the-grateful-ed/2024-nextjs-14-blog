c69c3b974d18d7b63538512bec3cf0a8e9ec643e
edndacomputer <edndacomputer@riseup.net>
2023-12-24 22:39:41 -0800

    hero

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/header';
+import Hero from '@/components/hero';
 import PostComponent from '@/components/post-component';
 import SocialIcons from '@/components/social-links';
 
@@ -31,24 +31,7 @@ export default async function Home() {
 
   return (
     <main className='relative'>
-      <div
-        className='relative overflow-hidden bg-cover bg-no-repeat rounded-t-xl'
-        style={{
-          backgroundPosition: '50%',
-          backgroundImage:
-            'url("https://tecdn.b-cdn.net/img/new/slides/146.webp")',
-          height: '350px',
-        }}
-      >
-        <div className='absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed'>
-          <div className='flex h-full items-center justify-center'>
-            <div className='px-6 text-center text-white md:px-12'>
-              <Header title='Articles' />
-              <h3 className='mb-8 text-3xl font-bold'>Subeading</h3>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Hero />
       <SocialIcons />
       <div className='space-y-4 overflow-hidden bg-accent px-2'>
         {posts?.length > 0 &&
