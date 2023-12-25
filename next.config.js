2c491578d7076339f9f9756fde7ff9203ea44f5d
edndacomputer <edndacomputer@riseup.net>
2023-12-24 22:06:42 -0800

    linting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+  };
